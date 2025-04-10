cc15b9ac0f3d17632be3a6027c0065e48d823562
arlan-6 <arlanhan1997@gmail.com>
2025-04-10 21:55:16 +0500

    fix: Update branding text from Shaqr to Baramyz in logo and footer

--- components/bottom-navigation.tsx
+++ components/bottom-navigation.tsx
@@ -32,7 +32,7 @@ export default function BottomNavigation() {
 
 				{/* Center Text */}
 				<div className="text-center text-sm w-full md:w-auto">
-					Invite was created with ❤️ in Shaqr App
+					Invite was created with ❤️ in Baramyz App
 				</div></div>
 
 				{/* Right Section */}
